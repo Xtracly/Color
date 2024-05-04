258ba3c1c57dcfc4b448ddfe45f0e5c453e217e8
Dima Sokhin <netlizard1@gmail.com>
2024-05-04 11:51:54 +0300

    Update arr

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let btn = document.getElementById('btn')
 let text = document.getElementById('text')
 
-let arr = ['#023245', '#452365', '#954320', '#34f231', '#863a32']
+let arr = ['#023245', '#452365', '#954320', '#34f231', '#863a32', '#ff3267']
 
 btn.addEventListener('click', function() {
     let rand = arr[Math.floor(Math.random() * 5)]
