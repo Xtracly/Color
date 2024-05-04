0c49848765dab3e3ec7413f070417e04fc6217b7
Dima Sokhin <netlizard1@gmail.com>
2024-05-04 11:57:59 +0300

    Update let rand

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let text = document.getElementById('text')
 let arr = ['#023245', '#452365', '#954320', '#34f231', '#863a32', '#ff3267']
 
 btn.addEventListener('click', function() {
-    let rand = arr[Math.floor(Math.random() * 5)]
+    let rand = arr[Math.floor(Math.random() * arr.length)]
     text.innerHTML = `Background Color: ${rand}`
     document.body.style.backgroundColor = rand
 })
